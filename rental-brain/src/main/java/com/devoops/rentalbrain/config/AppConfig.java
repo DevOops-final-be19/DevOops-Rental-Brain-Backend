aa0a6a6a1f54a48c3e0ed0baf3b1f137d0afcaf3
thdgudtjr0415 <thdgudtjr0415@gmail.com>
2025-12-11 16:07:07 +0900

    no message

--- rental-brain/src/main/java/com/devoops/rentalbrain/config/AppConfig.java
+++ rental-brain/src/main/java/com/devoops/rentalbrain/config/AppConfig.java
@@ -1,11 +1,19 @@
 package com.devoops.rentalbrain.config;
 
+
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+
 @Configuration
 public class AppConfig {
+//    @Bean
+//    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+//        return new  BCryptPasswordEncoder();
+//    }
+
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
