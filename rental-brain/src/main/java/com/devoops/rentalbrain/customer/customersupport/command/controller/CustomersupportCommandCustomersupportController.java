9b9bbb8f421a7251853f585319a36fb290b57ba9
SoonWooLee1 <lsw010420@gmail.com>
2025-12-12 15:44:10 +0900

    고객 문의, 피드백 쿼리 작성 중

--- rental-brain/src/main/java/com/devoops/rentalbrain/customer/customersupport/command/controller/CustomersupportCommandCustomersupportController.java
+++ rental-brain/src/main/java/com/devoops/rentalbrain/customer/customersupport/command/controller/CustomersupportCommandCustomersupportController.java
@@ -2,25 +2,29 @@ package com.devoops.rentalbrain.customer.customersupport.command.controller;
 
 import com.devoops.rentalbrain.customer.customersupport.command.dto.CustomersupportDTO;
 import com.devoops.rentalbrain.customer.customersupport.command.service.CustomersupportCommandCustomersupportService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/customer-supports")
 public class CustomersupportCommandCustomersupportController {
 
     private final CustomersupportCommandCustomersupportService commandService;
 
-    @PostMapping
+    @PostMapping("/insertSupport")
     public ResponseEntity<Long> register(@RequestBody CustomersupportDTO dto) {
         return ResponseEntity.ok(commandService.registerSupport(dto));
     }
 
-    @PutMapping("/{id}")
+    @PutMapping("/updateSupport/{id}")
     public ResponseEntity<String> update(@PathVariable Long id, @RequestBody CustomersupportDTO dto) {
         commandService.updateSupport(id, dto);
         return ResponseEntity.ok("문의가 수정되었습니다.");
     }
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping("/deleteSupport/{id}")
     public ResponseEntity<String> delete(@PathVariable Long id) {
         commandService.deleteSupport(id);
         return ResponseEntity.ok("문의가 삭제되었습니다.");
--- rental-brain/src/main/java/com/devoops/rentalbrain/customer/customersupport/command/controller/CustomersupportCommandFeedbackController.java
+++ rental-brain/src/main/java/com/devoops/rentalbrain/customer/customersupport/command/controller/CustomersupportCommandFeedbackController.java
@@ -13,18 +13,18 @@ public class CustomersupportCommandFeedbackController {
 
     private final CustomersupportCommandFeedbackService commandService;
 
-    @PostMapping
+    @PostMapping("/insertFeedback")
     public ResponseEntity<Long> register(@RequestBody FeedbackDTO dto) {
         return ResponseEntity.ok(commandService.registerFeedback(dto));
     }
 
-    @PutMapping("/{id}")
+    @PutMapping("/updateFeedback/{id}")
     public ResponseEntity<String> update(@PathVariable Long id, @RequestBody FeedbackDTO dto) {
         commandService.updateFeedback(id, dto);
         return ResponseEntity.ok("피드백이 수정되었습니다.");
     }
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping("/deleteFeedback/{id}")
     public ResponseEntity<String> delete(@PathVariable Long id) {
         commandService.deleteFeedback(id);
         return ResponseEntity.ok("피드백이 삭제되었습니다.");
