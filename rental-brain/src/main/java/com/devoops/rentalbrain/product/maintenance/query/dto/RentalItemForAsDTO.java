e7534cd86f685befcc85d0f386508606155b34ac
huni2 <powerhch@gmail.com>
2025-12-19 17:16:35 +0900

    feat: #57 고객별 렌탈 중인 자산목록 조회 기능 추가

---
+++ rental-brain/src/main/java/com/devoops/rentalbrain/product/maintenance/query/dto/RentalItemForAsDTO.java
@@ -0,0 +1,19 @@
+package com.devoops.rentalbrain.product.maintenance.query.dto;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class RentalItemForAsDTO {
+
+    private String itemCode;
+    private String itemName;
+    private String itemStatus;
+    private Integer repairCost;
+    private LocalDateTime lastInspectDate;
+}
