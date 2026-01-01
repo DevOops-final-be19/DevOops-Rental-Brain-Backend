4e8aca75ed08aac3fba63461af26d06369a8cb15
Kjandgo <rptmffld0204@gmail.com>
2026-01-01 16:22:15 +0900

    health log

--- rental-brain/src/main/java/com/devoops/rentalbrain/common/healthcheck/HealthController.java
+++ rental-brain/src/main/java/com/devoops/rentalbrain/common/healthcheck/HealthController.java
@@ -1,12 +1,15 @@
 package com.devoops.rentalbrain.common.healthcheck;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class HealthController {
     @GetMapping("/health")
     public String healthCheck() {
+        log.info("health check");
         return "I'm OK";
     }
 }
